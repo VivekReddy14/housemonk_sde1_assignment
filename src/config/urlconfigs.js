79b67a15d9c50e86167d2fbe7269c2f4e2a47fdd
Vivek-1409 <f20180481@hyderabad.bits-pilani.ac.in>
2023-03-21 21:29:43 +0530

    Mongo db url

--- src/config/urlconfigs.js
+++ src/config/urlconfigs.js
@@ -1,5 +1,5 @@
 module.exports = {
     redirecturl : "http://localhost:4200", // Home page of of web application
     webhookbaseurl : "https://invoicepay.onrender.com", // ngrok url of our service localhost:9899
-    mongodbconnectionurl : "mongodb://localhost:27017/invoicedb"
+    mongodbconnectionurl : "mongodb://u8ugytxpcruoo2qkkshx:Kh7xMl2bz1NGTf4KeSl2@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bpfvn5j36efpf1d?replicaSet=rs0"
 }
