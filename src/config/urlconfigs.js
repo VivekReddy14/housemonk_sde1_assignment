b41651ede7abdc0809ba95556e37607e236b6782
Vivek-1409 <f20180481@hyderabad.bits-pilani.ac.in>
2023-03-21 19:26:57 +0530

    changinf url

--- src/config/urlconfigs.js
+++ src/config/urlconfigs.js
@@ -1,5 +1,5 @@
 module.exports = {
     redirecturl : "http://localhost:4200", // Home page of of web application
-    webhookbaseurl : "https://83fc-2406-7400-9a-1e92-5556-af51-b70c-cd44.in.ngrok.io", // ngrok url of our service localhost:9899
+    webhookbaseurl : "https://invoicepay.onrender.com", // ngrok url of our service localhost:9899
     mongodbconnectionurl : "mongodb://localhost:27017/invoicedb"
 }
